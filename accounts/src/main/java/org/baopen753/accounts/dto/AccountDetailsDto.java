f0365dbc062ff119f15c34c16c6a31aa36ae0eec
baopen753 <tranquocbao070503@gmail.com>
2025-04-29 00:15:39 +0700

    modify: record to class for modifying at runtime

--- accounts/src/main/java/org/baopen753/accounts/dto/AccountDetailsDto.java
+++ accounts/src/main/java/org/baopen753/accounts/dto/AccountDetailsDto.java
@@ -2,10 +2,15 @@ package org.baopen753.accounts.dto;
 
 import java.util.HashMap;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-
 @ConfigurationProperties(prefix = "accounts")
-public record AccountDetailsDto(String message, HashMap<String, String> contactDetails, List<String> calls) {
-    
+@Getter
+@Setter
+public class AccountDetailsDto {
+    private String message;
+    private HashMap<String, String> contactDetails;
+    private List<String> calls;
 }
