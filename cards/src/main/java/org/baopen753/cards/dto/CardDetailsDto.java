cd21fe64d072337a1adb9b1f09d2610716317336
baopen753 <tranquocbao070503@gmail.com>
2025-04-29 00:34:11 +0700

    modify: setters & getters method

--- cards/src/main/java/org/baopen753/cards/dto/CardDetailsDto.java
+++ cards/src/main/java/org/baopen753/cards/dto/CardDetailsDto.java
@@ -1,11 +1,15 @@
 package org.baopen753.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.HashMap;
 import java.util.List;
 
 @ConfigurationProperties(prefix = "cards")
+@Getter
+@Setter
 public class CardDetailsDto{
     private String message;
     private HashMap<String, String> contactDetails;
--- loans/src/main/java/org/baopen753/loans/dto/LoanDetailDto.java
+++ loans/src/main/java/org/baopen753/loans/dto/LoanDetailDto.java
@@ -1,11 +1,15 @@
 package org.baopen753.loans.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.HashMap;
 import java.util.List;
 
 @ConfigurationProperties(prefix = "loans")
+@Getter
+@Setter
 public class LoanDetailDto {
     private String message;
     private HashMap<String, String> contactDetails;
